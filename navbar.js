2a6b4a04cbb06a264bc8e4c069fb2f1a87e813e4
Diksha <makkardiksha02@gmail.com>
2023-05-28 18:30:36 +0530

    Create navbar.js

---
+++ navbar.js
@@ -0,0 +1,32 @@
+// navbar.js
+
+// Function to generate the navigation bar
+function generateNavbar() {
+  var navbar = document.createElement('nav');
+  navbar.setAttribute('id', 'navbar');
+  
+  navbar.innerHTML = `
+    <div id="logo">
+      <img src="img/logo2.png" alt="WeAssureSecurity.com" />
+    </div>
+    <div class="vertical-line"></div>
+    <div class="logo-text">
+      <h3>WE ASSURE SECURITY</h3>
+    </div>
+    <ul>
+      <li class="item"><a href="index.html">Home</a></li>
+      <li class="item"><a href="#services-container">Services</a></li>
+      <li class="item"><a href="careers.html">Careers</a></li>
+      <li class="item"><a href="#contact">Contact Us</a></li>
+    </ul>
+  `;
+  document.body.insertBefore(navbar, document.body.firstChild);
+}
+
+// Call the generateNavbar function when the DOM is loaded
+document.addEventListener('DOMContentLoaded', generateNavbar);
+var favicon = document.createElement('link');
+  favicon.rel = 'icon';
+  favicon.type = 'image/png';
+  favicon.href = 'img/logo.png';
+  document.head.appendChild(favicon);
