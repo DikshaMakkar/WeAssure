f4a081160441638dc3ecd94d05ba80aa5b20db45
Diksha <makkardiksha02@gmail.com>
2023-05-28 18:29:34 +0530

    Create footer.js

---
+++ footer.js
@@ -0,0 +1,3 @@
+const footer = document.createElement('footer');
+footer.innerHTML = '<div class="center">Copyright &copy; www.weassuresecurity.com All rights reserved!</div>'
+document.body.appendChild(footer);
